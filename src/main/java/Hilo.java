013f846aee7e42cfaa108fde2f4b0325b0b23ada
Sergio <sergon149@alu.centroafuera.es>
2021-10-21 13:27:18 +0200

    primera parte echa

--- src/main/java/Hilo.java
+++ src/main/java/Hilo.java
@@ -1,35 +1,46 @@
+import org.w3c.dom.ls.LSOutput;
+
 import java.util.Random;
 
 public class Hilo extends Thread {
 
     int num = 0;
-    int dormir = 4000;
+    int dormir = 1000;
     Random rd = new Random();
+    boolean sino;
+    int cont;
+
+    long start = System.currentTimeMillis();
+
+
 
     public Hilo(int num) {
         this.num = num;
+        this.cont = 0;
     }
 
     public void run() {
-        System.out.println("He salido: "+rd.nextBoolean());
-        if (rd.nextBoolean() == true) {
-            try {
-                Thread.sleep(dormir+1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-            System.out.println("Soy el hilo numero " + num + " y he dormido por " + dormir + " milisegundos");
-        } else {
-            try {
+        try {
+            Thread.sleep(dormir);
+            cont=1;
+            sino=rd.nextBoolean();
+            while(sino==true) {
                 Thread.sleep(dormir);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+                sino=rd.nextBoolean();
+                cont++;
             }
-            System.out.println("Soy el hilo numero " + num + " y he dormido por " + dormir + " milisegundos");
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
+
+        long finish = System.currentTimeMillis();
+
+        long TiempoDormido = finish - start;
+
+        System.out.println("Soy el hilo numero " + num + " y he dormido " + cont + " veces y por "+TiempoDormido+" milisegundos");
     }
+
+
+
+
 }
-
-            //System.out.println("Soy el hilo numero "+num+" y he dormido por "+dormir+" milisegundos");
-            //System.out.println("He salido: "+rd.nextBoolean());
-
